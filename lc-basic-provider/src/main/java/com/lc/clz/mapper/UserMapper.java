5034036b0f159c4b4713a3fa1bf5c93dcff51066
chenglezheng <981797842@qq.com>
2019-01-05 23:54:31 +0800

    上一版本的补充提交

---
+++ lc-basic-provider/src/main/java/com/lc/clz/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lc.clz.mapper;
+
+import com.lc.clz.entities.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
