21b32637668eea08c9556ad3345544f4ee1b2462
chenglezheng <981797842@qq.com>
2019-04-26 16:53:17 +0800

    增加turbine集群监控，未调试成功，控制台看不到监控服务

--- lc-hystrix-monitor/src/main/java/com/lc/clz/HystrixMonitor.java
+++ lc-hystrix-monitor/src/main/java/com/lc/clz/HystrixMonitor.java
@@ -4,11 +4,13 @@ package com.lc.clz;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 import java.util.Scanner;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class HystrixMonitor {
 
     public static void main(String[] args) {
