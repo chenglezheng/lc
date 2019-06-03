08f07cb28108f348d5ba7e8f9f7e2583364e2a3d
lovefjy <jykjit@163.com>
2019-06-03 10:29:09 +0800

    新增BaseGoodInfo实体类

---
+++ lc-common/src/main/java/com/lc/clz/entities/BaseGoodInfo.java
@@ -0,0 +1,50 @@
+package com.lc.clz.entities;
+
+/**
+ * @author shenjiang
+ * @Description:
+ * @Date: 2019/5/11 13:00
+ */
+public class BaseGoodInfo {
+
+    private int bgiId;
+
+    private String bgiName;
+
+    private Double bgiPrice;
+
+    private String bgiUnit;
+
+
+    public int getBgiId() {
+        return bgiId;
+    }
+
+    public void setBgiId(int bgiId) {
+        this.bgiId = bgiId;
+    }
+
+    public String getBgiName() {
+        return bgiName;
+    }
+
+    public void setBgiName(String bgiName) {
+        this.bgiName = bgiName;
+    }
+
+    public Double getBgiPrice() {
+        return bgiPrice;
+    }
+
+    public void setBgiPrice(Double bgiPrice) {
+        this.bgiPrice = bgiPrice;
+    }
+
+    public String getBgiUnit() {
+        return bgiUnit;
+    }
+
+    public void setBgiUnit(String bgiUnit) {
+        this.bgiUnit = bgiUnit;
+    }
+}
