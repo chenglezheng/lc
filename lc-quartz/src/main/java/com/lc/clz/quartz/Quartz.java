a7c2206a0fc3962fa0738688c79e1119ada3310b
lovefjy <jykjit@163.com>
2019-06-04 11:09:39 +0800

    更改启动类文件名

--- lc-quartz/src/main/java/com/lc/clz/quartz/Quartz.java
+++ lc-quartz/src/main/java/com/lc/clz/quartz/Quartz.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class LcQuartzApplication {
+public class Quartz {
 
     public static void main(String[] args) {
-        SpringApplication.run(LcQuartzApplication.class, args);
+        SpringApplication.run(Quartz.class, args);
     }
 
 }
