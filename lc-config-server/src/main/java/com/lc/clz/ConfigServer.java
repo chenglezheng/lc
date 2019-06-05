a67d8be2d03be665c7adb22b9989642133864130
lovefjy <jykjit@163.com>
2019-06-05 22:11:57 +0800

    发射post请求

--- lc-config-server/src/main/java/com/lc/clz/ConfigServer.java
+++ lc-config-server/src/main/java/com/lc/clz/ConfigServer.java
@@ -5,12 +5,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient  //服务客户端，让该服务注册到Eureka中
 @EnableDiscoveryClient //服务发现
 @EnableConfigServer  //启动注册中心
 public class ConfigServer {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigServer.class).run(args);
---
+++ lc-config-server/src/main/java/com/lc/clz/postRequest/postRequest.java
@@ -0,0 +1,39 @@
+package com.lc.clz.postRequest;
+
+
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author shenjiang
+ * @Description:
+ * @Date: 2019/5/10 12:41
+ */
+@RestController
+@RefreshScope
+public class PostRequest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/sentPost")
+    public String Post(){
+        String sql = "http://localhost:8888/bus/refresh";
+        sendPost(null,sql);
+        return "哈哈";
+    }
+
+    public void sendPost(String param,String requestUrl){//发post请求
+
+        MultiValueMap<String, String> requestEntity = new LinkedMultiValueMap<>();
+        restTemplate.postForObject(requestUrl,requestEntity,String.class);
+    }
+
+}
