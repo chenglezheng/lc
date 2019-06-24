5ab83fed4cfd40198fcff421392d44c4bdc7a6bb
chenglezheng <981797842@qq.com>
2019-06-24 23:45:44 +0800

    1.认证中心能够正常获取token了，但是lc-user-center资源服务器无法根据token获取服务 2.修改了所有的url，将ip改成了.com地址，新增了一个host.txt，参考该配置在本机配置host信息； 3.删除了lc-invoker服务（此前是用于测试的）； 4.新增了lc-user-center服务，同时也是资源服务器；

---
+++ lc-user-center/src/main/java/com/lc/clz/UserCenter.java
@@ -0,0 +1,26 @@
+package com.lc.clz;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Scanner;
+
+/**
+ * Created by chenglezheng on 2018/12/28.
+ */
+@SpringBootApplication
+@EnableEurekaClient  //服务客户端，让该服务注册到Eureka中
+@EnableDiscoveryClient //服务发现
+@EnableTransactionManagement /*开启事务管理*/
+@MapperScan("com.lc.clz.dao")  //扫描mapper，否则启动会报找不到mapper
+public class UserCenter {
+
+
+    public static void main(String[] args){
+        new SpringApplicationBuilder(UserCenter.class).run(args);
+    }
+}
