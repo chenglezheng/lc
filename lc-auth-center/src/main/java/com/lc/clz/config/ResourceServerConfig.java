49bf576498c2025d06009304e51fc3d6b6082418
chenglezheng <981797842@qq.com>
2019-07-24 22:00:46 +0800

    swagger配置了全局的token认证，但是认证失败

--- lc-auth-center/src/main/java/com/lc/clz/config/ResourceServerConfig.java
+++ lc-auth-center/src/main/java/com/lc/clz/config/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package com.lc.clz.config;
 
+import com.lc.clz.utils.PermitUrlUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,13 +13,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-				.exceptionHandling()
-				.authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-				.and()
-				.authorizeRequests()
-				.anyRequest().authenticated()
-				.and()
-				.httpBasic();
+		http.csrf().disable().exceptionHandling()
+				.authenticationEntryPoint(
+						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+				.and().authorizeRequests()
+				.antMatchers(PermitUrlUtils.permitAllUrl()).permitAll() // 放开权限的url
+				.anyRequest().authenticated().and().httpBasic();
 	}
 }
--- lc-auth-center/src/main/java/com/lc/clz/config/SwaggerConfig.java
+++ lc-auth-center/src/main/java/com/lc/clz/config/SwaggerConfig.java
@@ -5,9 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +28,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.lc.clz.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
                 .apiInfo(
                     new ApiInfoBuilder()
                         .version("1.0")
@@ -29,5 +39,27 @@ public class SwaggerConfig {
                 );
     }
 
+
+    private List<ApiKey> securitySchemes() {
+        return newArrayList(
+                new ApiKey("Authorization", "Authorization", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build()
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(
+                new SecurityReference("Authorization", authorizationScopes));
+    }
 
 }
